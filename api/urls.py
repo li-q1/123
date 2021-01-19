0ab9ea2299a8a531dac2a2ccfd67fd61e982c1a1
çli-1' <1240996077@qq.com>
2021-01-19 21:25:55 +0800

    11

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_jwt import views as view
+
+from api import views
+
+urlpatterns = [
+    path("login/", view.ObtainJSONWebToken.as_view()),
+    path("users/", views.UserDetailAPIVIew.as_view()),
+    path("user/", views.LoginAPIView.as_view()),
+]
